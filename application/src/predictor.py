039e209a2b693d29487ae451725878502afa3c51
tekuprogramming <156584093+tekuprogramming@users.noreply.github.com>
2026-02-11 20:30:37 +0100

    added folder application with folder src with files main.py and predictor.py

---
+++ application/src/predictor.py
@@ -0,0 +1,9 @@
+# predictor.py - module for loading models and predicting the weather
+
+import pickle
+import numpy as np
+from datetime import datetime
+
+class AirQualityPredictor:
+    def __init__(self):
+        pass
