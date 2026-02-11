a12763e42af0edf045e8c56b8b30aaa52bba1564
tekuprogramming <156584093+tekuprogramming@users.noreply.github.com>
2026-02-11 19:50:08 +0100

    finished the method get_air_quality_data; added methods collect_and_save, calculate_aqi_category and run_continue

--- data_collection/collector.py
+++ data_collection/collector.py
@@ -35,12 +35,82 @@ class AirCollector:
 
     def get_air_quality_data(self):
         url = "https://api.openaq.org/v2/latest?limit=100&city=Prague&parameter=pm25&parameter=pm10&parameter=no2"
-        # try:
-            # response = requests.get(url, timeout=10)
-            # data = response.json()
-            # pm25 = None
-            # pm10 = None
-            # for result in data.get("results", []):
-                # for measurement in result.get("measurements", []):
-                    # if measurement.
+        try:
+            response = requests.get(url, timeout=10)
+            data = response.json()
+            pm25 = None
+            pm10 = None
+            for result in data.get("results", []):
+                for measurement in result.get("measurements", []):
+                    if measurement["parameter"] == "pm25":
+                        pm25 = measurement["value"]
+                    elif measurement["parameter"] == "pm10":
+                        pm10 = measurement["value"]
+            return {"pm25": pm25, "pm10": pm10}
+        except Exception as e:
+            print(f"Error getting air quality data: \n{e}")
+            return None
+
+    def collect_and_save(self):
+        timestamp = datetime.now()
+        weather = self.get_weather_data()
+        air_quality = self.get_air_quality_data()
+        if weather and air_quality:
+            record = {"timestamp": timestamp.isoformat(),
+                      "data": timestamp.date().isoformat(),
+                      "hour": timestamp.hour,
+                      "day_of_week": 1 if timestamp.weekday() >= 5 else 0,
+                      "temperature": weather["temperature"],
+                      "humidity": weather["humidity"],
+                      "pressure": weather["pressure"],
+                      "wind_speed": weather["wind_speed"],
+                      "wind_deg": weather["wind_deg"],
+                      "weather_main": weather["weather_main"],
+                      "clouds": weather["clouds"],
+                      "pm25": air_quality["pm25"],
+                      "pm10": air_quality["pm10"],
+                      "air_category": self.calculate_aqi_category(air_quality["pm25"])}
+            df = pd.DataFrame([record])
+            if os.path.exists(self.data_file):
+                df.to_csv(self.data_file, mode="a", header=False, index=False)
+            else:
+                df.to_csv(self.data_file, index = False)
+
+            print("Data saved to file:", timestamp)
+            return True
+        return False
+
+    # calculates the category of air quality for pm25
+    def calculate_aqi_category(self, pm25):
+        if pm25 is None:
+            return "Unknown"
+        elif pm25 <= 12:
+            return "Good"
+        elif pm25 <= 35:
+            return "Moderate"
+        elif pm25 <= 55:
+            return "Sensitive"
+        elif pm25 <= 150:
+            return "Unhealthy"
+        else:
+            return "Hazardous"
+
+    # start of indefinite data collection
+    def run_continue(self, interval_hours = 2):
+        print("Start of data collection for: ", self.city)
+        print("Data will be collected every 2 hours")
+        while True:
+            success = self.collect_and_save()
+            if success:
+                print(f"Data collected successfully, next collection in {interval_hours} hours")
+            else:
+                print(f"Data collection failed, next collection in 30 minutes")
+                time.sleep(1800) # 30 min
+            time.sleep(interval_hours * 3600) # interval between data collections
+
+if __name__ == "__main__":
+    collector = AirCollector()
+    collector.collect_and_save()
+    
+
 
